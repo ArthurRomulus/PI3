f6c529d3df941e905af7d2a8e9e616f17ee2fbe0
iNoxy10 <erickgerman170106@gmail.com>
2025-10-16 17:50:58 -0600

    cambios en la redireccion en login y registro

--- General/login.php
+++ General/login.php
@@ -3,7 +3,7 @@
 session_start();
 
 // Incluye el archivo de conexión a la base de datos
-include '../database.php';
+include '../conexion.php';
 
 // Variable para almacenar mensajes de error
 $error_message = '';
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                   header("Location: ../Cajero/Inicio/Inicio.html");
 
                 } else {
-                                  header("Location: ../Usuarios");
+                                  header("Location: ../Usuario/index.php");
 
                 }
                 
