f3f2e69b8fb1c2e4f963f48b8bb445faeb08fa4d
iNoxy10 <erickgerman170106@gmail.com>
2025-10-27 19:34:17 -0600

    Correcion bd yaa
    Added ON UPDATE CASCADE to foreign key constraint for idp.

--- tienda_db.sql
+++ tienda_db.sql
@@ -219,9 +219,10 @@ CREATE TABLE `producto_opciones` (
   `opciones` TEXT NOT NULL,
   PRIMARY KEY (`id_opcion`),
   KEY `idp` (`idp`),
-  CONSTRAINT `producto_opciones_ibfk_1` FOREIGN KEY (`idp`) 
-      REFERENCES `productos` (`idp`) ON DELETE CASCADE
+  CONSTRAINT `producto_opciones_ibfk_1` FOREIGN KEY (`idp`)
+      REFERENCES `productos` (`idp`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
 
 --
