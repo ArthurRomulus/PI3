a93e3248e6aece18a817fbc0e4e413109c941c69
Arthur7567 <miguepg06@gmail.com>
2025-11-24 06:59:22 -0600

    cd442

--- PHPMailer/PHPMailer.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * PHPMailer - PHP email creation and transport class.
- * @package PHPMailer
- * @see https://github.com/PHPMailer/PHPMailer/
- */
-// ...código original de PHPMailer...
